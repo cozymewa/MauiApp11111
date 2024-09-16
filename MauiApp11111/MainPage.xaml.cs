0c45dbb5e80fcfb8652694de90464752a8b94385
1900134 <1900134@TE-GDA-D-D21E16>
2024-09-16 09:37:16 +0200

    Dodaj pliki projektów.

---
+++ MauiApp11111/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp11111
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //wygenerowanie liczby losowej
+            Random r = new Random();
+            //genrujemy rzut kostką K6 - <1;6>
+            int diceRoll = r.Next(1, 7);
+            //wyświetlenie wyniku
+            RollValueLabel.Text = "Wysolowany rzut: " + diceRoll.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
